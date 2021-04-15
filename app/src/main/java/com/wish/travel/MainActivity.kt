963c5b8652db77378942b6e393ee3c90886b4ab2
Veronika Kudelkova <455499@mail.muni.cz>
2021-04-15 10:12:53 +0200

    Edit MainActivity

--- app/src/main/java/com/wish/travel/MainActivity.kt
+++ app/src/main/java/com/wish/travel/MainActivity.kt
@@ -1,6 +1,36 @@
 package com.wish.travel
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.wish.travel.databinding.ActivityMainBinding
+import com.wish.travel.ui.wishlist.WishlistFragment
 
-class MainActivity: AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        binding.bottomNavigation.setOnNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.action_wishlist -> {
+                    switchFragment(WishlistFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+
+        switchFragment(WishlistFragment())
+    }
+
+    private fun switchFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
+    }
 }
