0b7c9334e9a33f89c2da85d91746f6e3b308e224
Radoslav Čerhák <r.cerhak@gmail.com>
2021-05-14 10:52:19 +0200

    Redesigned searching in explore countries

--- app/src/main/java/com/wish/travel/ui/explore/ExploreFragment.kt
+++ app/src/main/java/com/wish/travel/ui/explore/ExploreFragment.kt
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.wish.travel.Communicator
 import com.wish.travel.R
+import com.wish.travel.data.Country
 import com.wish.travel.databinding.FragmentExploreBinding
 import com.wish.travel.repository.RestCountriesRepository
 import com.wish.travel.ui.country.CountryActivity
@@ -16,7 +17,7 @@ import com.wish.travel.util.toast
 class ExploreFragment: Fragment(R.layout.fragment_explore) {
 
     companion object {
-        val regions = listOf("None", "Africa", "Americas", "Asia", "Europe", "Oceania")
+        val regions = listOf("All", "Africa", "Americas", "Asia", "Europe", "Oceania")
     }
 
     private val restCountriesRepository = RestCountriesRepository()
@@ -39,10 +40,11 @@ class ExploreFragment: Fragment(R.layout.fragment_explore) {
         binding.exploreRecyclerView.adapter = adapter
         showAllCountries()
 
-//        binding.searchButton.setOnClickListener {
-//            val name = binding.searchByNameTextField.editText?.text.toString()
+        binding.searchButton.setOnClickListener {
+//            val name = binding.searchByNameInputField.editText?.text.toString()
 //            showCountriesByName(name)
-//        }
+            searchCountries()
+        }
 
         initRegionsSpinner()
     }
@@ -51,18 +53,54 @@ class ExploreFragment: Fragment(R.layout.fragment_explore) {
         val regionsAdapter =
             ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, regions)
         binding.regionSpinner.adapter = regionsAdapter
-        binding.regionSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onNothingSelected(p0: AdapterView<*>?) {}
+//        binding.regionSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+//            override fun onNothingSelected(p0: AdapterView<*>?) {}
+//
+//            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, position: Int, p3: Long) {
+//                val selectedRegion = regions[position]
+//                if (position == 0) {
+//                    showAllCountries()
+//                } else {
+//                    showCountriesInRegion(selectedRegion)
+//                }
+//            }
+//        }
+    }
 
-            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, position: Int, p3: Long) {
-                val selectedRegion = regions[position]
-                if (position == 0) {
-                    showAllCountries()
-                } else {
-                    showCountriesInRegion(selectedRegion)
-                }
+    private fun searchCountries() {
+        val selectedRegionPosition = binding.regionSpinner.selectedItemPosition
+        val searchName = binding.searchByNameInputField.editText?.text.toString()
+
+        // no region selected
+        if (selectedRegionPosition == 0) {
+            if (searchName.isEmpty()) {
+                showAllCountries()
+            } else {
+                showCountriesByName(searchName)
+            }
+        } else {
+            if (searchName.isEmpty()) {
+                showCountriesInRegion(regions[selectedRegionPosition])
+            } else {
+                showCountriesByNameAndRegion(searchName, regions[selectedRegionPosition])
             }
         }
+    }
+
+    private fun showCountriesByNameAndRegion(name: String, region: String) {
+        restCountriesRepository.getCountriesByName(
+            name,
+            successCallback = { countries ->
+                val filteredCountries = countries.filter { country -> country.region.equals(region) }
+                if (filteredCountries.isEmpty()) {
+                    noCountriesFound()
+                }
+                adapter.submistList(filteredCountries)
+                (activity as Communicator).passCountries(ArrayList(filteredCountries))
+            },
+            failureCallback = {
+                noCountriesFound()
+            })
     }
 
     private fun showCountriesByName(name: String) {
@@ -73,8 +111,7 @@ class ExploreFragment: Fragment(R.layout.fragment_explore) {
                 (activity as Communicator).passCountries(ArrayList(countries))
             },
             failureCallback = {
-                adapter.submistList(emptyList())
-                activity?.toast(R.string.explore_no_countries_found)
+                noCountriesFound()
             })
     }
 
@@ -86,8 +123,7 @@ class ExploreFragment: Fragment(R.layout.fragment_explore) {
                 (activity as Communicator).passCountries(ArrayList(countries))
             },
             failureCallback = {
-                adapter.submistList(emptyList())
-                activity?.toast(R.string.explore_no_countries_found)
+                noCountriesFound()
             })
     }
 
@@ -98,8 +134,12 @@ class ExploreFragment: Fragment(R.layout.fragment_explore) {
                 (activity as Communicator).passCountries(ArrayList(countries))
             },
             failureCallback = {
-                adapter.submistList(emptyList())
-                activity?.toast(R.string.explore_no_countries_found)
+                noCountriesFound()
             })
+    }
+
+    private fun noCountriesFound() {
+        adapter.submistList(emptyList())
+        activity?.toast(R.string.explore_no_countries_found)
     }
 }
